bca9f81c81f227227dac16fb7c1a0ee4a0b2cf38
kikenna <44933361+kikenna@users.noreply.github.com>
2021-03-03 18:12:26 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -349,7 +349,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} ♛".format("♛", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} ◆".format("◆", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -365,10 +365,10 @@ def paginate_help(page_number, loaded_modules, prefix):
         ] + [
             (
                 custom.Button.inline(
-                    "☚", data="{}_prev({})".format(prefix, modulo_page)
+                    "◄", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "☛", data="{}_next({})".format(prefix, modulo_page)
+                    "►", data="{}_next({})".format(prefix, modulo_page)
                 )
             )
         ]
@@ -403,8 +403,8 @@ with bot:
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.article(
                     "Harap Gunakan .help Untuk Perintah",
-                    text="{}\n\n**❃ Jumlah Modul Yang Tersedia:** `{}`\n               \n**❃ Daftar Modul Lord Userbot:** \n".format(
-                        "**♛ Lord-Userbot**",
+                    text="{}\n\n**✶ Jumlah Modul Yang Tersedia:** `{}`\n               \n**✶ Daftar Modul Lord Userbot:** \n".format(
+                        "**✶ Lord-Userbot**",
                         len(dugmeler),
                     ),
                     buttons=buttons,
