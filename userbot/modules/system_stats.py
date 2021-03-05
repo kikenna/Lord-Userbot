27aef405cb3b07463c30d3ae35e3a44167877043
yahiko963 <44933361+yahiko963@users.noreply.github.com>
2021-03-06 02:22:37 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -261,16 +261,16 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
-        f"**▬▬▬▬▬▬❙۩♛۩❙▬▬▬▬▬▬**\n"
-        f"     **♕ LORD USERBOT ♕** \n\n"
-        f"❃ **Lord**     \n   ➥ `{DEFAULTUSER}` \n"
-        f"❃ **Username** \n   ➥ `@{user.username}` \n"
-        f"❃ **Telethon** \n   ➥ `Versi {version.__version__}` \n"
-        f"❃ **Python**   \n   ➥ `Versi {python_version()}` \n"
-        f"❃ **Versi Bot**\n   ➥ `{BOT_VER}` \n"
-        f"❃ **Modul**    \n   ➥ `{len(modules)}` \n\n"
-        f"❃ **Repo Userbot:** [Lord-Userbot](https://github.com/Zora24/Lord-Userbot)\n❃ **Grup Userbot: **[Tekan](t.me/LordUserbot_Group)\n❃ **Pemilik:** [Alvin](t.me/liualvinas)\n"
-        f"**▬▬▬▬▬▬❙۩★۩❙▬▬▬▬▬▬**")
+        f"**▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁**\n"
+        f"     **- NEWBIE USERBOT -** \n\n"
+        f"◆ **Lord**     \n   ➨ `{DEFAULTUSER}` \n"
+        f"◆ **Username** \n   ➨ `@{user.username}` \n"
+        f"◆ **Telethon** \n   ➨ `Versi {version.__version__}` \n"
+        f"◆ **Python**   \n   ➨ `Versi {python_version()}` \n"
+        f"◆ **Versi Bot**\n   ➨ `{BOT_VER}` \n"
+        f"◆ **Modul**    \n   ➨ `{len(modules)}` \n\n"
+        f"◆ **Repo Userbot:** [NewbieBot](https://github.com/yahiko963/Lord-Userbot\n◆ **Grup Userbot: **[Tekan](t.me/LordUserbot_Group)\n◆ **Pemilik:** [Frisky](t.me/Lubego666)\n"
+        f"**▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ **")
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
