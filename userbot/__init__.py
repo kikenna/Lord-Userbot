2ff845efa68111f2185111d267ed45898a10c1b6
yahiko963 <44933361+yahiko963@users.noreply.github.com>
2021-03-06 12:55:20 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -319,7 +319,7 @@ with bot:
 
 
 async def check_alive():
-    await bot.send_message(BOTLOG_CHATID, "```『Lord-Userbot Telah Aktif』```")
+    await bot.send_message(BOTLOG_CHATID, "```『Newbie-Bot Siap Digunakan』```")
     return
 
 with bot:
@@ -349,7 +349,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} ".format("", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} ◆".format("◆", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -390,7 +390,7 @@ with bot:
         @tgbot.on(events.NewMessage(pattern="/start"))
         async def handler(event):
             if event.message.from_id != uid:
-                await event.reply("Lord-Userbot, Buat Userbot Mu Sendiri [Tekan Disini](https://github.com/Zora24/Lord-Userbot.git)")
+                await event.reply("Newbie-bot, Buat Userbot Mu Sendiri [Tekan Disini](https://github.com/Yahiko963/Lord-Userbot.git)")
             else:
                 await event.reply(f"`Hai Lord {ALIVE_NAME}\n\nApa Kabarmu?`")
 
@@ -403,8 +403,8 @@ with bot:
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.article(
                     "Harap Gunakan .help Untuk Perintah",
-                    text="{}\n\n** Jumlah Modul Yang Tersedia:** `{}`\n               \n**✶ Daftar Modul Lord Userbot:** \n".format(
-                        "** Newbie-bot**",
+                    text="{}\n\n** Jumlah Modul Yang Tersedia:** `{}`\n               \n** Daftar Modul Lord Userbot:** \n".format(
+                        "**Newbie-bot**",
                         len(dugmeler),
                     ),
                     buttons=buttons,
